b097d5e0225cef0f9f26e342787c0d9e3cb6ff9d
Vlados <vbura2000@gmail.com>
2025-06-19 15:50:17 +0300

    Исправления

--- public/homePage.js
+++ public/homePage.js
@@ -1,94 +1,94 @@
-"use strict"
-
-const { response } = require("express");
+"use strict";
 
 const objLogoutButton = new LogoutButton();
 
-objLogoutButton.action = () => ApiConnector.logout((response) => {
-    if(response.success){
-        return location.reload();
+objLogoutButton.action = () =>
+  ApiConnector.logout((response) => {
+    if (response.success) {
+      return location.reload();
     }
-  }
-);
+  });
 
-ApiConnector.current(response => {
-    if(response.success){
-        ProfileWidget.showProfile(response.data);
-    }
-})
+ApiConnector.current((response) => {
+  if (response.success) {
+    ProfileWidget.showProfile(response.data);
+  }
+});
 
 const objRatesBoard = new RatesBoard();
 
-const getStocks = () => 
-    ApiConnector.getStocks(response => {
-    if(response.success){
-        objRatesBoard.clearTable();
-        objRatesBoard.fillTable(response.data)
+const getStocks = () =>
+  ApiConnector.getStocks((response) => {
+    if (response.success) {
+      objRatesBoard.clearTable();
+      objRatesBoard.fillTable(response.data);
     }
-})
+  });
 
 getStocks();
 
 setInterval(getStocks, 60000);
 
 const objMoneyManager = new MoneyManager();
-const messageError = new ProfileWidget();
 
 objMoneyManager.addMoneyCallback = (data) => {
-    ApiConnector.addMoney(data, response => {
-        if(response.success){
-            messageError.showProfile(response.data)
-        }
-        objMoneyManager.setMessage(response.success, response.error) // уточнить момент с ошибкой
-    });
+  ApiConnector.addMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+    }
+    objMoneyManager.setMessage(); 
+  });
 };
 
 objMoneyManager.conversionMoneyCallback = (data) => {
-    ApiConnector.convertMoney(data, response => {
-        if(response.success){
-            messageError.showProfile(response.data);
-        }
-        objMoneyManager.setMessage(response.success, response.error);
-    });
-}
+  ApiConnector.convertMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+    }
+    objMoneyManager.setMessage();
+  });
+};
 
 objMoneyManager.sendMoneyCallback = (data) => {
-    ApiConnector.transferMoney(data, response => {
-        if(response.success){
-            messageError.showProfile(response.data)
-        }
-        objMoneyManager.setMessage(response.success, response.error)
-    })
-}
+  ApiConnector.transferMoney(data, (response) => {
+    if (response.success) {
+      ProfileWidget.showProfile(response.data);
+    }
+    objMoneyManager.setMessage();
+  });
+};
 
 const objFavoritesWidget = new FavoritesWidget();
 
-ApiConnector.getFavorites(response => {
-    if (response.success){
-        objFavoritesWidget.clearTable();
-        objFavoritesWidget.fillTable(response);
-        objMoneyManager.updateUsersList(response);
-    }
-})
+ApiConnector.getFavorites((response) => {
+  console.log(response)
+  if (response.success) {
+    objFavoritesWidget.clearTable();
+    objFavoritesWidget.fillTable();
+    objMoneyManager.updateUsersList(response);
+  }
+});
 
 objFavoritesWidget.addUserCallback = (data) => {
-    ApiConnector.addUserToFavorites(data, response => {
-    if (response.success){
-        objFavoritesWidget.clearTable();
-        objFavoritesWidget.fillTable(response);
-        objMoneyManager.updateUsersList(response);
+  ApiConnector.addUserToFavorites(data, (response) => {
+    console.log(response)
+    if (response.success) {
+      objFavoritesWidget.clearTable();
+      objFavoritesWidget.fillTable();
+      objMoneyManager.updateUsersList(response);
     }
-    objFavoritesWidget.setMessage(response.success, response.error)
-    })
-}
+    objFavoritesWidget.setMessage();
+  });
+};
 
 objFavoritesWidget.removeUserCallback = (data) => {
-    ApiConnector.removeUserFromFavorites(data, response => {
-         if (response.success){
-        objFavoritesWidget.clearTable();
-        objFavoritesWidget.fillTable(response);
-        objMoneyManager.updateUsersList(response);
+  ApiConnector.removeUserFromFavorites(data, (response) => {
+    console.log(response)
+    if (response.success) {
+      objFavoritesWidget.clearTable();
+      objFavoritesWidget.fillTable();
+      objMoneyManager.updateUsersList(response);
     }
-    objFavoritesWidget.setMessage(response.success, response.error)
-    })
-}
+    objFavoritesWidget.setMessage();
+  });
+};
